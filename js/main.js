c95c8d7d4694b172d1d8079cb7f499b9e9b245d3
realstate <aboamer0548@gmail.com>
2022-10-28 22:24:11 +0200

    update

--- js/main.js
+++ js/main.js
@@ -24,14 +24,63 @@ switcherLis.forEach((li) => {
 });
 
 // popup image
-let myimages = Array.from(document.querySelectorAll(".images img"));
+let myImages = Array.from(document.querySelectorAll(".images img"));
 let boxPort = document.querySelector(".box-portfolio");
-for (let i = 0; i < myimages.length; i++) {
-  myimages[i].addEventListener("click", shoBox);
+let text = document.querySelectorAll(".text-img ");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let wind = document.getElementById("window");
+let plus = document.getElementById("plus");
+let min = document.getElementById("min");
+let download = document.getElementById("download");
+let imgIndex = 0;
+for (let i = 0; i < myImages.length; i++) {
+  myImages[i].addEventListener("click", shoBox);
 }
-
+// show box
 function shoBox(e) {
   boxPort.style.transform = "scale(1,1)";
   let imgSrc = e.target.src;
   boxPort.firstElementChild.style.backgroundImage = "url(" + imgSrc + ")";
+  imgIndex = myImages.indexOf(e.target);
 }
+//close box
+window.onclick = (e) => {
+  if (e.target == boxPort) {
+    boxPort.style.transform = "scale(0,0)";
+  }
+};
+//next box
+next.addEventListener("click", nextBox);
+function nextBox() {
+  imgIndex++;
+  if (imgIndex == myImages.length) {
+    imgIndex = 0;
+  }
+  boxPort.firstElementChild.style.backgroundImage = `url(${myImages[imgIndex].src})`;
+}
+// previes box
+prev.addEventListener("click", prevBox);
+function prevBox() {
+  imgIndex--;
+  if (imgIndex < 0) {
+    imgIndex = myImages.length - 1;
+  }
+  boxPort.firstElementChild.style.backgroundImage = `url(${myImages[imgIndex].src})`;
+}
+// full screen
+wind.addEventListener("click", windowBox);
+function windowBox() {
+  boxPort.style.transform = "scale(3,1.9)";
+}
+// zoom out
+plus.addEventListener("click", plusBox);
+function plusBox() {
+
+}
+// zoom in
+min.addEventListener("click", minBox);
+function minBox() {}
+// zoom out
+download.addEventListener("click", downloadBox);
+function downloadBox() {}
